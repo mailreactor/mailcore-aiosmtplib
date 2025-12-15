cfafdd1683b5c14864d649b9b584313689dea5e7
Hans Christian von Stockhausen <hans.vonstockhausen@bancon-it.com>
2025-12-15 18:16:58 +0200

    E3S11.0 bootstrap done

---
+++ src/mailcore_aiosmtplib/__init__.py
@@ -0,0 +1,7 @@
+"""mailcore-aiosmtplib - aiosmtplib adapter for mailcore."""
+
+__version__ = "0.1.0"
+
+# Exports will be populated in Story 3.13
+# from .adapter import AIOSMTPAdapter
+# __all__ = ["AIOSMTPAdapter"]
